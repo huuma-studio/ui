b26a5ef85b5869adf5a4833e0a26704b2671896b
Daniel Steuri <daniel.steuri@flowingones.ch>
2025-02-07 16:10:31 +0100

    chore: remove old files

--- old/cargo/bundler/deps.ts
+++
@@ -1,4 +0,0 @@
-import * as esbuild from "https://deno.land/x/esbuild@v0.18.20/mod.js";
-export { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.8.1/mod.ts";
-
-export { esbuild };
--- old/cargo/constants.ts
+++
@@ -1,2 +0,0 @@
-export const BUILD_ID = Deno.env.get("DENO_DEPLOYMENT_ID") ??
-  crypto.randomUUID().replaceAll("-", "");
