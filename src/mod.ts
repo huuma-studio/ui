ebd271add82baed9fb9679a9a4af210657d34576
Daniel Steuri <daniel.steuri@flowingones.ch>
2025-06-08 14:06:14 +0200

    chore: export more types

---
+++ src/mod.ts
@@ -0,0 +1,3 @@
+import type { JSX } from "./jsx-runtime/mod.ts";
+
+export type Props = JSX.ComponentProps;
--- src/platform/server/mod.ts
+++ src/platform/server/mod.ts
@@ -1,4 +1,4 @@
 export { renderToString, vNodeToString } from "./render.ts";
 export { Launch, Scripts } from "./scripts.ts";
 export { Meta } from "./meta.ts";
-export { createUIApp, UIApp } from "./app.ts";
+export { createUIApp, type PageLike, type PageProps, UIApp } from "./app.ts";
